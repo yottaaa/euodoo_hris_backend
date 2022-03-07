1c81aed49456fcb5692bc4801272be14d704a460
Erick A. Bautista <yotterick7@gmail.com>
2022-03-07 17:01:02 +0800

    update views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 
-# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+from knox.views import LoginView as KnoxLoginView
+
+from account.serializers import UserSerializer
+
+def register(request):
+    pass
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get_post_response_data(self, request, token, instance):
+        UserSerializer = self.get_user_serializer_class()
+
+        data = {
+            'expiry': self.format_expiry_datetime(instance.expiry),
+            'token': token
+        }
+        if UserSerializer is not None:
+            data['user'] = UserSerializer(
+                request.user,
+                context=self.get_context()
+            ).data
+        
+        return data
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
