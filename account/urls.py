7a2224328702d6f6345b8a79a2ef3afd1a1c9818
Erick A. Bautista <yotterick7@gmail.com>
2022-03-07 17:03:47 +0800

    add urls

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from knox import views as knox_views
+from account import views as acc_views
+
+urlpatterns = [
+    path('login/', acc_views.LoginAPI.as_view(), name='user-login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='user-logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='user-logoutall'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
