327f234e31deba115d0aeaf68d294631a88d3bbe
Erick A. Bautista <yotterick7@gmail.com>
2022-03-07 17:02:39 +0800

    update settings

--- hris_backend/settings.py
+++ hris_backend/settings.py
@@ -57,10 +57,10 @@ REST_KNOX = {
   'SECURE_HASH_ALGORITHM': 'cryptography.hazmat.primitives.hashes.SHA512',
   'AUTH_TOKEN_CHARACTER_LENGTH': 64,
   'TOKEN_TTL': timedelta(hours=10),
-  'USER_SERIALIZER': 'knox.serializers.UserSerializer',
+  'USER_SERIALIZER': 'account.serializers.UserSerializer',
   'TOKEN_LIMIT_PER_USER': None,
   'AUTO_REFRESH': False,
-  'EXPIRY_DATETIME_FORMAT': api_settings.DATETME_FORMAT,
+  'EXPIRY_DATETIME_FORMAT': api_settings.DATETIME_FORMAT,
 }
 
 MIDDLEWARE = [
